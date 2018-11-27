d0a3e3c66e868c9ba31cf3dbcb9b4c099ef3008d
Andrew Hao <andrewhao@gmail.com>
2018-11-26 21:44:29 -0800

    Updating HomePage with new typography and logo

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,20 @@
 import * as React from "react";
-import { Grid, Paper, withStyles, Typography, Button } from "@material-ui/core";
+import {
+  Grid,
+  Paper,
+  withStyles,
+  Typography,
+  Button,
+  ExpansionPanel,
+  ExpansionPanelSummary,
+  ExpansionPanelDetails
+} from "@material-ui/core";
 import CsvFileUpload from "../CsvFileUpload";
 import { IAmazonOrderItem } from "../types/data";
 import * as R from "ramda";
 import { isoDateToFriendlyDisplay } from "../util/DateUtils";
+import Glasses from "../images/glasses.svg";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 export interface IHomePageProps {
   classes: any;
@@ -61,59 +72,106 @@ function HomePage({
 
   return (
     <div>
+      <Grid container={true}>
+        <Grid item={true} xs={12}>
+          <h1
+            className="hero"
+            style={{
+              fontFamily: "Shrikhand",
+              fontSize: "5rem",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              fontWeight: "normal",
+              marginBottom: 0,
+              marginTop: 0
+            }}
+          >
+            <div className="hero__text">Spendyvision</div>
+            <img
+              className="hero__logo"
+              style={{ marginLeft: "3rem", height: "5rem" }}
+              src={Glasses}
+            />
+          </h1>
+        </Grid>
+        <Grid container={true}>
+          <Grid item={true} xs={2} />
+          <Grid item={true} xs={8}>
+            <Typography variant="headline" gutterBottom={true} align="center">
+              Spendyvision helps you get a grip on your Amazon spending by
+              showing you what you're spending on and how it's changing over
+              time.
+            </Typography>
+          </Grid>
+          <Grid item={true} xs={2} />
+        </Grid>
+      </Grid>
       <Grid item={true} xs={12}>
         {loadedItemMsg(sortedItems)}
         <Paper className={classes.paper}>
+          <Typography variant="headline">Get started</Typography>
           <label htmlFor="csv-file-upload__input">
-            <Typography variant="headline" gutterBottom={true}>
-              Upload a recent order report
+            <Typography variant="subheading" gutterBottom={true}>
+              Download your Amazon Order Report CSV and upload it here.
             </Typography>
           </label>
 
           <CsvFileUpload handleCsvUpload={handleCsvUpload} />
+          <Typography variant="body1">
+            (This data is stored on this browser, never on our servers.)
+          </Typography>
         </Paper>
       </Grid>
       <Grid item={true} xs={12}>
-        <Paper className={classes.paper}>
-          <Typography variant="headline" gutterBottom={true}>
-            How to download your Amazon Order Report
-          </Typography>
-          <Typography variant="title" gutterBottom={true}>
-            1. Visit the Amazon.com Order History Report page
-          </Typography>
-          <p>
-            Visit the{" "}
-            <a href="https://www.amazon.com/gp/b2b/reports">
-              Order History Report Page
-            </a>
-          </p>
-          <Typography variant="title" gutterBottom={true}>
-            2. Download an "Items" Order Report to your computer.
-          </Typography>
+        <ExpansionPanel>
+          <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+            <Typography variant="headline" gutterBottom={true}>
+              How to download your Amazon Order Report
+            </Typography>
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <Grid item={true} xs={12}>
+              <Typography variant="title" gutterBottom={true}>
+                1. Visit the Amazon.com Order History Report page
+              </Typography>
+              <p>
+                Visit the{" "}
+                <a href="https://www.amazon.com/gp/b2b/reports">
+                  Order History Report Page
+                </a>
+              </p>
+              <Typography variant="title" gutterBottom={true}>
+                2. Download an "Items" Order Report to your computer.
+              </Typography>
 
-          <p>
-            <img
-              src="/images/amazon-download-report.gif"
-              width="90%"
-              alt="Screencast of report download"
-            />
-          </p>
-          <p>
-            Choose which dates you want to import in your report, then click
-            "Request Report". The file should download to your computer.
-          </p>
-          <p>
-            <strong>Important:</strong> Make sure that the "Report Type" you
-            download is <strong>"Items"</strong>
-          </p>
+              <p>
+                <img
+                  src="/images/amazon-download-report.gif"
+                  width="90%"
+                  alt="Screencast of report download"
+                />
+              </p>
+              <p>
+                Choose which dates you want to import in your report, then click
+                "Request Report". The file should download to your computer.
+              </p>
+              <p>
+                <strong>Important:</strong> Make sure that the "Report Type" you
+                download is <strong>"Items"</strong>
+              </p>
 
-          <Typography variant="title" gutterBottom={true}>
-            3. Click the "Upload Amazon Order Report" and select your report.
-          </Typography>
-          <p>
-            Spendyvision will automatically load the data in this application.
-          </p>
-        </Paper>
+              <Typography variant="title" gutterBottom={true}>
+                3. Click the "Upload Amazon Order Report" and select your
+                report.
+              </Typography>
+              <p>
+                Spendyvision will automatically load the data in this
+                application.
+              </p>
+            </Grid>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
       </Grid>
     </div>
   );
