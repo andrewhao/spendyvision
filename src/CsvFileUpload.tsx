eecbe20d94e0c19424f73ba700475d876129804d
Andrew Hao <andrewhao@gmail.com>
2018-12-25 22:34:18 -0800

    Redirect behaviors go on Upload widget

--- src/CsvFileUpload.tsx
+++ src/CsvFileUpload.tsx
@@ -1,12 +1,16 @@
 import * as React from "react";
 import { Button, withStyles } from "@material-ui/core";
 import CSVReader from "react-csv-reader";
+import { withRouter } from "react-router-dom";
 
 type ICsvUploadFn = (results: any[], filename: string) => void;
 
 interface ICsvFileUploadProps {
   handleCsvUpload: ICsvUploadFn;
   classes: any;
+  history: any;
+  location: any;
+  match: any;
 }
 
 const styles: any = {
@@ -16,11 +20,16 @@ const styles: any = {
 };
 
 function CsvFileUpload(props: ICsvFileUploadProps) {
+  const { history } = props;
+  const uploadAndRedirect: ICsvUploadFn = (results, filename) => {
+    props.handleCsvUpload(results, filename);
+    history.push("/summary");
+  };
   return (
     <div className="csv-file-upload" style={{ display: "inline-block" }}>
       <CSVReader
         label="Upload Amazon Order Report"
-        onFileLoaded={props.handleCsvUpload}
+        onFileLoaded={uploadAndRedirect}
         cssClass={props.classes.fileInput}
         inputId="csv-file-upload__input"
       />
@@ -33,4 +42,4 @@ function CsvFileUpload(props: ICsvFileUploadProps) {
   );
 }
 
-export default withStyles(styles)(CsvFileUpload);
+export default withRouter(withStyles(styles)(CsvFileUpload));
